d55d733bfa05168ee73dc9101c93bf6986aaed56
Cat Allen <catallen@pm.me>
2020-05-30 13:33:56 -0500

    Code is put together but having errors. Checking to see if i'm missing some import/exports.

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,28 +1,47 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { CSSTransitionGroup } from 'react-transition-group';
 import Question from "../components/Question";
 import QuestionCount from "../components/QuestionCount";
 import AnswerOption from '../components/AnswerOption';
 
 function Quiz(props) {
-    return (
-        <div className="quiz">
-            <QuestionCount
-                counter={props.questionId}
-                total={props.questionTotal}
+    function renderAnswerOptions(key) {
+        return (
+            <AnswerOption
+                key={key.content}
+                answerContent={key.content}
+                answerType={key.type}
+                answer={props.answer}
+                questionId={props.questionId}
+                onAnswerSelected={props.onAnswerSelected}
             />
-            <Question content={props.question} />
-            <ul className="answerOptions">
-                {props.answerOptions.map(renderAnswerOptions)}
-            </ul>
-        </div>
+        );
+    }
+    return (
+        <CSSTransitionGroup
+            className="container"
+            component="div"
+            transitionName="fade"
+            transitionEnterTimeout={800}
+            transitionLeaveTimeout={500}
+            transitionAppear
+            transitionAppearTimeout={500}
+        >
+            <div key={props.questionId}>
+                <QuestionCount counter={props.questionId} total={props.questionTotal} />
+                <Question content={props.question} />
+                <ul className="answerOptions">
+                    {props.answerOptions.map(renderAnswerOptions)}
+                </ul>
+            </div>
+        </CSSTransitionGroup>
     );
 }
 
 Quiz.propTypes = {
     answer: PropTypes.string.isRequired,
     answerOptions: PropTypes.array.isRequired,
-    counter: PropTypes.number.isRequired,
     question: PropTypes.string.isRequired,
     questionId: PropTypes.number.isRequired,
     questionTotal: PropTypes.number.isRequired,
